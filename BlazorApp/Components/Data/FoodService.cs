af409a456441b93df0412145d4e7ef857d6dccf4
igh01gi <igh01pr@outlook.com>
2024-01-22 07:57:56 +0900

    DI 코딩 테스트 완료
    //1. AddSingleton
    //2. AddTransient
    //3. AddScoped

---
+++ BlazorApp/Components/Data/FoodService.cs
@@ -0,0 +1,109 @@
+﻿namespace BlazorApp.Components.Data;
+
+public class FoodService
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    
+    public interface IFoodService
+    {
+        IEnumerable<Food> GetFoods();
+    }
+    
+    public class KoreanFoodService : IFoodService
+    {
+        public IEnumerable<Food> GetFoods()
+        {
+            return new List<Food>
+            {
+                new Food { Name = "Kimchi", Price = 10000 },
+                new Food { Name = "Bibimbap", Price = 8000 },
+                new Food { Name = "Bulgogi", Price = 15000 },
+                new Food { Name = "Samgyeopsal", Price = 20000 },
+                new Food { Name = "Japchae", Price = 10000 },
+            };
+        }
+    }
+    
+    public class FastFoodService : IFoodService
+    {
+        public IEnumerable<Food> GetFoods()
+        {
+            return new List<Food>
+            {
+                new Food { Name = "Hamburger", Price = 5000 },
+                new Food { Name = "Cheeseburger", Price = 6000 },
+                new Food { Name = "French Fries", Price = 3000 },
+                new Food { Name = "Chicken Nuggets", Price = 4000 },
+                new Food { Name = "Onion Rings", Price = 4000 },
+            };
+        }
+    }
+    
+    public class PaymentService
+    {
+        private IFoodService _service;
+        
+        public PaymentService(IFoodService service)  //스프링처럼 자동 DI 해줌 (Program.cs 에서 IFoodService의 구현체로 설정한것이 자동으로 DI 됨. 그러기 위해서는 PaymentService도 싱글톤등록(스프링빈 같은) 해줘야 함)
+        {
+            _service = service;
+        }
+        
+        //TODO
+    }
+    
+    # region 스프링빈 같은것의 생명주기 3가지 모드 테스트
+    
+    public class SingletonService : IDisposable
+    {
+        public Guid ID { get; set; }
+
+        public SingletonService()
+        {
+            ID = Guid.NewGuid();
+        }
+        
+        public void Dispose()
+        {
+            Console.WriteLine($"SingletonService disposed");
+        }
+    }
+    
+    public class TransientService : IDisposable
+    {
+        public Guid ID { get; set; }
+
+        public TransientService()
+        {
+            ID = Guid.NewGuid();
+        }
+        
+        public void Dispose()
+        {
+            Console.WriteLine($"TransientService disposed");
+        }
+    }
+    
+    public class ScopedService : IDisposable
+    {
+        public Guid ID { get; set; }
+
+        public ScopedService()
+        {
+            ID = Guid.NewGuid();
+        }
+        
+        public void Dispose()
+        {
+            Console.WriteLine($"ScopedService disposed");
+        }
+    }
+    
+    # endregion
+    
+
+    
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,10 +1,32 @@
 using BlazorApp.Components;
+using BlazorApp.Components.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+#region DI 설정
+
+//DI 설정
+builder.Services.AddSingleton<FoodService.IFoodService, FoodService.KoreanFoodService>();
+builder.Services.AddSingleton<FoodService.PaymentService>(); //생성자에서 알아서 연결 해줌 (스프링과 같이)
+//스프링빈 같은것을 생성할때 3가지 모드
+//1. AddSingleton 
+//2. AddTransient 
+//3. AddScoped 
+// https://afsdzvcx123.tistory.com/entry/ASPNET-Core-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%83%9D%EB%AA%85%EC%A3%BC%EA%B8%B0-Singleton-Scoped-Transient
+// https://velog.io/@zeehyo2216/Blazor-DI-Singleton-Scoped-Transient 참고
+//https://learn.microsoft.com/ko-kr/aspnet/core/blazor/fundamentals/dependency-injection?view=aspnetcore-8.0
+builder.Services.AddSingleton<FoodService.SingletonService>();
+builder.Services.AddTransient<FoodService.TransientService>();
+builder.Services.AddScoped<FoodService.ScopedService>();
+
+
+#endregion
+
 
 var app = builder.Build();
 
