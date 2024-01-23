7efc9791ab85e9caccda68d4aba57ce29e74d59c
igh <igh01pr@outlook.com>
2024-01-23 11:58:58 +0900

    validation 하는 방법 및 EditForm 사용

---
+++ BlazorApp/Components/Data/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Components.Data;
+
+public class TestClass
+{
+    public DateOnly Date { get; set; }
+
+    [Required(ErrorMessage = "Need TemperatureC")]
+    [Range(typeof(int), "-100", "100")]
+    public int TemperatureC { get; set; }
+
+    [Required(ErrorMessage = "Need Summary")]
+    [StringLength(10, MinimumLength = 2, ErrorMessage = "2~10글자가능")]
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
